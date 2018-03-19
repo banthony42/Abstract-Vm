79de21606e755dcedfec36d1fc1bc37505a453d1
Anthony BARA <banthony@e2r9p18.42.fr>
2018-03-19 17:39:35 +0100

    Debut du parseur

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,62 @@
 #include <vector>
 #include <sstream>
 
-std::vector<std::string> *get_script(char *filename)
+/*
+ * Chaque ligne du programme correspond a un element d'un tableau de tableau. (vector de vector de string)
+ * Chaque ligne est stockee sous forme de vector de string, issue d'un strsplit sur les espaces.
+ *
+ * Consideront un programme simple, et une variable tab, qui est un tableau, de tableau de string. (std::vector< std::vector<std::string> > )
+ *
+ * push int32(42) ;commentaire   =  tab[0]
+ *                                      tab[0].begin()      = push
+ *                                      tab[0].begin() + 1  = int32(42)
+ *                                      tab[0].begin() + 2  = ;commentaire
+ *
+ * push int32(21)               =   tab[1]
+ *                                      tab[1].begin()      = push
+ *                                      tab[1].begin() + 1  = int32(21)
+ *
+ * dump                         =   tab[2]
+ *                                      tab[2].begin()      = dump
+ *
+ * exit                         =   tab[3]
+ *                                      tab[3].begin()      = exit
+ */
+
+typedef std::vector< std::vector<std::string> > vector_vstr;
+typedef std::vector< std::string > vector_str;
+
+#define DEBUG(str) std::cout << str << std::endl
+
+std::vector<std::string> my_strsplit(std::string str)
+{
+    std::vector<std::string> tab;
+    char *cstr = const_cast<char *>(str.c_str());
+    char *tmp;
+
+    tmp = strtok (cstr," ");
+    while (tmp != NULL)
+    {
+        tab.push_back(tmp);
+        tmp = strtok (NULL, " ");
+    }
+    return (tab);
+}
+
+vector_vstr *get_script(char *filename)
 {
     std::ifstream file;
     std::string buf;
-    std::vector<std::string> *config = new std::vector<std::string>;
+    vector_vstr *config = new vector_vstr;
 
     file.open(filename, std::ifstream::in);
     if (file.is_open())
     {
-        std::cout << "file is open" << std::endl << std::endl;
+        DEBUG("file is open");
         while (getline(file, buf))
         {
             if (!buf.empty())
-                config->push_back(buf);
+                config->push_back(my_strsplit(buf));
         }
         file.close();
         return (config);
@@ -36,53 +78,49 @@ std::vector<std::string> *get_script(char *filename)
     return (NULL);
 }
 
-std::vector<std::string> *get_script_by_stdin(void)
+vector_vstr *get_script_by_stdin(void)
 {
-    std::vector<std::string> *config = NULL;
-    config = new std::vector<std::string>;
+    vector_vstr *tab = new vector_vstr;
+    vector_vstr::const_iterator it;
+    vector_str::const_iterator tmp;
+    std::string buf;
 
-    /**** SIMULATION ENTREE CLAVIER ****/
-
-    config->push_back(";");
-    config->push_back("; FAKE STDIN SCRIPT");
-    config->push_back(";");
-
-    config->push_back("push int8(10)");
-    config->push_back("push int8(10)");
-    config->push_back("add");
-    config->push_back("exit");
-    config->push_back(";;");
-
-    return (config);
+    while(std::cin.good()) {
+        getline(std::cin, buf);
+        if (!buf.empty())
+            tab->push_back(my_strsplit(buf));
+        if (tab->empty() == false)
+        {
+            it = tab->end() - 1;                                /* Recuperation du dernier vector_str (derniere entree utilisateur) */
+            tmp = find(it->begin(), it->end(), ";;");           /* Recherche de la séquence ";;" (Fin entree utilisateur) */
+            if (tmp != it->end())                               /* Si ";;" est trouvé on stop la saisi */
+                break ;
+        }
+    }
+    return (tab);
 }
 
 int main(int ac, char **av)
 {
-    std::vector<std::string> *config = NULL;
-    std::vector<std::string>::const_iterator it;
+    vector_vstr *config = NULL;
+    vector_vstr::const_iterator it;
 
-    if (!(config = get_script(av[1])))
+    if (ac > 1) {
+        if (!(config = get_script(av[1])))
+            return (0);
+    }
+    else
         config = get_script_by_stdin();
 
     it = config->begin();
-    while (it != config->end())
-    {
-        std::cout << *it << std::endl;
+
+    while (it != config->end()) {
+        if (it->size())
+            std::cout << it->front() << std::endl;
         it++;
     }
 
+    delete config;
 
-    char str[] ="This,a sample string coucou salut les amis";
-    char * pch;
-    printf ("Splitting string \"%s\" into tokens:\n",str);
-    pch = strtok (str," ,.-");
-    while (pch != NULL)
-    {
-        printf ("%s\n",pch);
-        pch = strtok (NULL, " ");
-    }
-
-
-    (void)ac;
     return (0);
 }
